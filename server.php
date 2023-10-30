c4fedd5106e926d406b65a2f9413b02f2c9358b5
AnotherDebug <antonio.faddanno@gmail.com>
2023-10-30 17:33:56 +0100

    [ADD]: decode encode json

--- server.php
+++ server.php
@@ -1,5 +1,20 @@
 <?php
 
-
+//prendo la lista json, la trasformo in stringa e la salvo in una variabile.
 $json_string = file_get_contents('todoList.json');
 
+//trasformo la stringa in un array php e la salvo in una variabile.
+$list = json_decode($json_string);
+
+
+
+
+
+
+
+
+//trasformo l'array php in stringa.
+header('Content-Type: application=json');
+
+//stampo la lista trasformata da array php a stringa.
+echo json_encode($list);
