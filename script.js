81ff567a9a727d6c9eff1b6a0c8b95285a741648
AnotherDebug <antonio.faddanno@gmail.com>
2023-10-31 11:44:47 +0100

    [ADD]: deleteTask

--- script.js
+++ script.js
@@ -1,41 +1,57 @@
 const { createApp } = Vue;
 
 createApp({
-    data() {
-        return {
-            prova:'ciao',
-            list: [],
-            newTask: ''
-        }
+  data() {
+    return {
+      list: [],
+      newTask: "",
+    };
+  },
+  methods: {
+    getTodoList() {
+      console.log("lista");
+      axios
+        .get("server.php")
+        .then((res) => {
+          this.list = res.data;
+        })
+        .catch((e) => {
+          console.log(e);
+        });
     },
-    methods:{
-        getTodoList() {
-            console.log('lista');
-            axios.get('server.php')
-            .then(res => {
-                this.list = res.data;
-            })
-            .catch(e => {
-                console.log(e);
-            })
-        },
-        addTask(){
-            console.log('task aggiunto');
-            const data = new FormData();
-            data.append('taskItem', this.newTask );
+    addTask() {
+      console.log("task aggiunto");
+      const data = new FormData();
+      data.append("taskItem", this.newTask);
 
-            axios.post('server.php', data)
-            .then(res => {
-                this.list = res.data;
-                console.log(this.list);
-            })
-            .catch(e => {
-                console.log(e);
-            })
-        }
+      axios
+        .post("server.php", data)
+        .then((res) => {
+          this.list = res.data;
+          console.log(this.list);
+          this.newTask = "";
+        })
+        .catch((e) => {
+          console.log(e);
+        });
     },
-    mounted() {
-        console.log('prova');
-        this.getTodoList();
+    deleteTask(index) {
+        console.log('task eliminato');
+        const data = new FormData();
+        data.append("deleteItem", index);
+        axios
+        .post("server.php", data)
+        .then((res) => {
+          this.list = res.data;
+          console.log(this.list);
+        })
+        .catch((e) => {
+          console.log(e);
+        });
     }
-}).mount('#data');
+  },
+  mounted() {
+    console.log("prova");
+    this.getTodoList();
+  },
+}).mount("#data");
